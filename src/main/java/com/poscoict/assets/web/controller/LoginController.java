e977b523f22939f628a238afd0d9088ae32ee459
yoongdoo <http://yoongdoo0819@almcm.poscoict.com>
2019-12-29 12:12:52 +0900

    controller mint add

--- src/main/java/com/poscoict/assets/web/controller/LoginController.java
+++ src/main/java/com/poscoict/assets/web/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.poscoict.assets.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.poscoict.assets.chaincode.EERC721;
+import com.poscoict.assets.chaincode.ERC721;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,25 +28,41 @@ import com.poscoict.posledger.chain.sign.model.PosCertificate;
 import com.poscoict.posledger.chain.sign.model.PosCertificateMeta;
 import com.poscoict.posledger.chain.wallet.chaincode.ERC20ChaincodeService;
 
+import java.io.FileInputStream;
+import java.math.BigInteger;
+
 @Controller
 public class LoginController extends ExceptionHandleController {
 	
 	private static final Logger logger = LogManager.getLogger(LoginController.class);
 	
-	//@Autowired
+	@Autowired
 	private UserService userService;
 
-	//@Autowired
+	@Autowired
 	private PosCertificateService posCertificateService;
 	
-	//@Autowired
+	@Autowired
 	private ERC20ChaincodeService erc20ChaincodeService;
 	
-	//@Autowired
+	@Autowired
 	private ObjectMapper objectMapper;
 	
-	//@Autowired
+	@Autowired
 	private MessageSourceAccessor message;
+
+	@Autowired
+	ERC721 erc721;
+
+	@Autowired
+	EERC721 eerc721;
+
+	private String alice;
+	private String bob;
+	private String carol;
+	private String david;
+	private final BigInteger tokenId = BigInteger.ZERO;
+	private final static String CERT_PASSWARD = "1234";
 
 	/**
 	 * 회원 가입
@@ -134,5 +152,54 @@ public class LoginController extends ExceptionHandleController {
 
 		return new HttpResponse(HttpResponse.success, posCertificateMeta.getOwnerId());
 		
+	}
+
+	@RequestMapping(value="/ERC721/mint", method=RequestMethod.POST)
+	@ResponseBody
+	public HttpResponse mint(@RequestParam("certfile") MultipartFile certfile, HttpServletRequest request) throws RestResourceException, Exception {
+
+		//String fileName = "./certForAlice";
+		//MultipartFile certfile = new MockMultipartFile(fileName, new FileInputStream(fileName));
+
+		PosCertificate posCertificate = null;
+		try {
+			posCertificate = objectMapper.readValue(certfile.getBytes(), new TypeReference<PosCertificate>(){});
+		} catch(Exception e) {
+			logger.error(e);
+			throw new RestResourceException("유효하지 않은 인증서 형식입니다.");
+		}
+
+		// 인증서 비밀번호 검증
+		boolean isPassward = false;
+
+		try {
+			isPassward = posCertificateService.verifyPosCertificatePassword(posCertificate, CERT_PASSWARD);
+		} catch(Exception e) {
+			logger.error(e);
+			throw new RestResourceException("인증서 비밀번호를 확인해주세요.");
+		}
+
+		PosCertificateMeta posCertificateMeta = null;
+
+		if (isPassward) {
+			try {
+				posCertificateMeta = posCertificateService.getMobilePosCertificateMeta(posCertificate, CERT_PASSWARD, message.getMessage("application.posledger.challenge.domain"));
+			} catch (Exception e) {
+				logger.error(e);
+				throw new RestResourceException(e.getLocalizedMessage());
+			}
+		}
+
+		try {
+			alice = posCertificateMeta.getOwnerKey();
+		} catch (NullPointerException e) {
+			logger.error(e);
+			throw new NullPointerException(e.getLocalizedMessage());
+		}
+
+		erc721.setCaller(alice);
+		boolean result = erc721.mint(tokenId, alice);
+
+		return new HttpResponse(HttpResponse.success, "SUCCESS");
 	}
 }
