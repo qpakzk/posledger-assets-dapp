feab390778db1bd96ffe6e3005aae0d457575f64
Sangwon Hong <sangwonhong@poscoict.com>
2020-01-22 06:59:53 +0900

    delete unused source code in StandardController

--- src/main/java/com/poscoict/assets/web/controller/StandardController.java
+++ src/main/java/com/poscoict/assets/web/controller/StandardController.java
@@ -1,47 +1,17 @@
 package com.poscoict.assets.web.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.Http;
 import com.poscoict.assets.persistence.UserDao;
 import com.poscoict.posledger.chain.assets.chaincode.standard.*;
-import com.poscoict.assets.service.UserService;
 import com.poscoict.posledger.chain.assets.chaincode.util.Manager;
-import com.poscoict.posledger.chain.sign.certificate.PosCertificateService;
-import com.poscoict.posledger.chain.wallet.chaincode.ERC20ChaincodeService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.MessageSourceAccessor;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
 public class StandardController extends RootController {
-    private static final Logger logger = LogManager.getLogger(StandardController.class);
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private PosCertificateService posCertificateService;
-
-    @Autowired
-    private ERC20ChaincodeService erc20ChaincodeService;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private MessageSourceAccessor message;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
     @Autowired
     private UserDao userDao;
 
